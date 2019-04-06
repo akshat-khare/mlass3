39f5772d119f0cb91374339c42cbb22b552b5284
akshat-khare <akshat.khare08@gmail.com>
2019-04-07 00:23:14 +0530

    try has data

--- try.py
+++ try.py
@@ -1,23 +1,187 @@
-# one hot encoding
-count=0
-onehotxtest = []
-for i in range(23):
-    if(xidentifier[i]!=2):
-        onehotxtest.append([])
-        for j in range(numtest):
-            onehotxtest[count].append(xarrtest[i][j])
-        count+=1
-    else:
-        tempcat = xnumchildhelper[i]
-        for j in range(tempcat):
-            onehotxtest.append([])
-        for j in range(numtest):
-            tempval = xarrtest[i][j]
-            for k in range(tempcat):
-                if(k==tempval):
-                    onehotxtest[count+k].append(1)
+import numpy as np
+import math
+class Node:
+    def __init__(self,numtheta):
+        self.theta=[0.0]*numtheta
+        self.downstream=None
+        self.upstream=None
+        self.output=None
+        self.deljthetabydelthetaunit = [0.0]*numtheta
+        self.deljthetabydelnet = None
+def loadtheta(network,theta ):
+    for i in range(len(network)):
+        for j in range(len(network[i])):
+            for k in range(len(network[i][j].theta)):
+                network[i][j].theta[k] = theta[i][j][k]
+    return network
+def sigmoid(x):
+    return (1.0/(1.0+math.exp(-x)))
+def forwardpass(features, network):
+    for i in range(len(network)):
+        for j in range(len(network[i])):
+            tempnode = network[i][j]
+            temp=0.0
+            if(i==0):
+                #parse from input
+                for k in range(len(tempnode.theta)):
+                    if(k==len(tempnode.theta)-1):
+                        temp += tempnode.theta[k] * 1.0
+                    else:
+                        temp += tempnode.theta[k] * features[k]
+                
+            else:
+                #parse from last layer
+                prevlayer = network[i-1]
+                for k in range(len(tempnode.theta)):
+                    if(k==len(tempnode.theta)-1):
+                        temp += tempnode.theta[k] * 1.0
+                    else:
+                        temp += tempnode.theta[k] * prevlayer[k].output
+            tempnode.output = sigmoid(temp)
+    lastlayer = network[len(network)-1]
+    templist=[]
+    for i in range(len(lastlayer)):
+        templist.append(lastlayer[i].output)
+    return templist
+def findcost(output, correctoutput):
+    temp = 0.0
+    for i in range(len(output)):
+        temp += 0.5 * ((output[i]-correctoutput[i])**2)
+    return temp
+def findtotalcostarr(inparr,outarr,network):
+    costarr=[]
+    for i in range(len(inparr)):
+        tempforwardpass= forwardpass(inparr[i],network)
+        tempcost = findcost(tempforwardpass,tempforwardpass)
+        costarr.append(tempcost)
+    avgcost=0.0
+    for i in range(len(costarr)):
+        avgcost += costarr[i]
+    avgcost = avgcost/(len(costarr))
+    return costarr, avgcost
+def neuralnet(inparr, outarr, hiddeninfo,learningrate, epochsize,costthres, sampletheta):
+    numtrain = len(inparr)
+    diminput= len(inparr[0])
+    dimoutput = len(outarr[0])
+    network=[]
+    for i in range(len(hiddeninfo)):
+        network.append([])
+    network.append([])
+    for i in range(len(hiddeninfo)):
+        #i is layer iterator
+        for j in range(hiddeninfo[i]):
+            #j is sublayer num unit iterator
+            if(i==0):
+                tempnode = Node(diminput+1)
+                network[i].append(tempnode)
+            else:
+                tempnode = Node(hiddeninfo[i-1]+1)
+                network[i].append(tempnode)
+            
+    #add output layer
+    for i in range(dimoutput):
+        tempnode = Node(hiddeninfo[len(hiddeninfo)-1] + 1)
+        network[len(network)-1].append(tempnode)
+    #load theta for testing
+#     network = loadtheta(network, sampletheta)
+    #necessary forwardpass
+#     tempforwardpass = forwardpass(inparr[0],network)
+#     print(tempforwardpass)
+#     #lets see cost
+#     print("cost = "+str(findcost(tempforwardpass,outarr[0])))
+    
+    #time for backprop
+    deljthetabydelthetamat=[]
+    for i in range(len(network)):
+        deljthetabydelthetamat.append([])
+        for j in range(len(network[i])):
+            deljthetabydelthetamat[i].append([])
+            for k in range(len(network[i][j].theta)):
+                deljthetabydelthetamat[i][j].append(0.0)
+#     print(deljthetabydelthetamat)
+    subepochiter=0
+    epochcount=0
+    numiter=0
+    costarr,oldavgcost = findtotalcostarr(inparr,outarr,network)
+    
+    while(0==0):
+        numiter+=1
+        print("numiter is")
+        print(numiter)
+        ioindex = epochsize*epochcount+subepochiter
+        print("ioindex is "+str(ioindex))
+        tempforwardpass = forwardpass(inparr[ioindex],network)
+        for i in range(len(network)-1,-1,-1):
+            #lets calculate deljtheta by del netj
+            for j in range(len(network[i])):
+                tempnode = network[i][j]
+                if(i==len(network)-1):
+                    tempnode.deljthetabydelnet = -1.0*(outarr[ioindex][j]-tempnode.output)*(tempnode.output)*(1.0-tempnode.output)
                 else:
-                    onehotxtest[count+k].append(0)
-        count += tempcat
-templen=len(onehotxtest)
-onehotxtest = np.array(onehotxtest).reshape((templen,numtest))
+                    temp=0.0
+                    nextlayer = network[i+1]
+                    for k in range(len(nextlayer)):
+                        temp += nextlayer[k].deljthetabydelnet * nextlayer[k].theta[j]
+                    tempnode.deljthetabydelnet = (tempnode.output) * (1.0- tempnode.output) * temp
+                if(i==0):
+                    #ok is input
+                    for k in range(len(tempnode.theta)):
+                        if(k==len(tempnode.theta)-1):
+                            tempnode.deljthetabydelthetaunit[k] = tempnode.deljthetabydelnet * 1.0
+                        else:
+                            tempnode.deljthetabydelthetaunit[k] = tempnode.deljthetabydelnet * inparr[ioindex][k]
+                else:
+                    prevlayer = network[i-1]
+                    for k in range(len(tempnode.theta)):
+                        if(k==len(tempnode.theta)-1):
+                            tempnode.deljthetabydelthetaunit[k] = tempnode.deljthetabydelnet * 1.0
+                        else:
+                            tempnode.deljthetabydelthetaunit[k] = tempnode.deljthetabydelnet * prevlayer[k].output
+        subepochiter+=1
+        for i in range(len(network)):
+                for j in range(len(network[i])):
+                    for k in range(len(network[i][j].theta)):
+                        deljthetabydelthetamat[i][j][k] += network[i][j].deljthetabydelthetaunit[k]
+        print(deljthetabydelthetamat)
+        if(subepochiter==epochsize):
+            print("epoch over")
+            for i in range(len(network)):
+                for j in range(len(network[i])):
+                    for k in range(len(network[i][j].theta)):
+                        network[i][j].theta[k] = network[i][j].theta[k] - learningrate * deljthetabydelthetamat[i][j][k]/(1.0*epochsize)
+            #set deljthetabydel zero
+            costarr, newavgcost=findtotalcostarr(inparr,outarr,network)
+            print(newavgcost)
+            if(newavgcost<costthres):
+                print("cost is lower")
+                break
+            elif(numiter>=10):
+                break
+            else:
+                oldavgcost=newavgcost
+                subepochiter=0
+                epochcount+=1
+        
+            
+            
+            
+#         tempforwardpass = forwardpass(inparr[0],network)
+#         print(tempforwardpass)
+#         #lets see cost
+#         print("cost = "+str(findcost(tempforwardpass,outarr[0])))        
+    
+    return network
+sampleinparr = [[0.05, 0.10]]
+# sampleinparr = [[0.0, 1.0]]
+sampleoutput = [[0.01, 0.99]]
+# sampleoutput = [[0.0]]
+samplehiddeninfo = [2]
+samplelearningrate=0.5
+sampletheta = [[[.15,.20,.35],[.25,.30,.35]],[[.40,.45,.60],[.50,.55,.60]]]
+
+samplenetwork = neuralnet(sampleinparr,sampleoutput,samplehiddeninfo,samplelearningrate,1, pow(10,-3),sampletheta)
+print(samplenetwork)
+            
+                
+    
+    
